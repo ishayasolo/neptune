0ed19291b977502f3a34fb286cb70ad1eb5ae3b1
ishayasolo <ishaya808@gmail.com>
2023-01-09 14:42:50 +0100

    complete hero desktop interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "components/Hero"
 import NavBar from "components/NavBar"
 
 const App = () => {
   return (
-    <div className="max-w-7xl py-8 px-4 mx-auto">
+    <div className="max-w-7xl py-8 mx-auto">
       <NavBar />
+      <Hero />
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,33 @@
+import heroImage from "assets/svg/hero-photo.svg"
+import Button from "./Button"
+
 const Hero = () => {
-	return (
-		<div className="flex items-center gap-28">
-			
-		</div>
-	)
+  return (
+    <div
+      className="flex items-center justify-between gap-28"
+      style={{ height: "calc(100vh - 130px)" }}
+    >
+      <div className="flex flex-col gap-8">
+        <h1 className="text-4xl max-w-[315.5px] flex flex-col gap-4">
+          <span className="font-bold">Neptune</span>
+          <span className="font-bold">maritime industry</span>
+          <span>at your fingertips</span>
+        </h1>
+        <p className="leading-8 text-neptune-secondary-gray">
+          Neptune allow users to access a database of marine vehicles available
+          for rental
+        </p>
+        <div className="pt-8 flex gap-4">
+          {["Get started", "See our plans"].map(cta => (
+            <Button key={cta} value={cta} />
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-center items-center -mr-5">
+        <img src={heroImage} alt="" className="w-full" />
+      </div>
+    </div>
+  )
 }
 
 export default Hero

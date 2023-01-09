ad372c4c738f3fb9bac72483d9bc6b94dbc8980d
ishayasolo <ishaya808@gmail.com>
2023-01-09 02:49:57 +0100

    construct navigation bar

---
+++ src/utils/constants.js
@@ -0,0 +1,18 @@
+export const NAV_LINKS = [
+  {
+    label: "About Us",
+    path: "#about-us",
+  },
+  {
+    label: "Pricing",
+    path: "#pricing",
+  },
+  {
+    label: "How It Works",
+    path: "#how-it-works",
+  },
+  {
+    label: "Contact",
+    path: "#contact",
+  },
+]
